db2c432cfc7aec2919b5f981d3d1caca8aacf092
RonnaTechnologies <contact@ronnatech.com>
2024-11-17 15:22:11 +0100

    Add execute overload.

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 
 #include "sqlight.hpp"
 
-int main() {
+int main()
+{
 
-  using sqlight::db;
-  using sqlight::query;
+    using sqlight::db;
+    using sqlight::query;
 
-  db database{":memory:"};
-  database.exec("CREATE TABLE IF NOT EXISTS test(id INTEGER NOT NULL, name "
-                "TEXT NOT NULL);");
+    db database{ ":memory:" };
+    database.execute("CREATE TABLE IF NOT EXISTS test(id INTEGER NOT NULL, name TEXT NOT NULL);");
 
-  database.exec("INSERT INTO test VALUES (1, 'abc');");
-  database.exec("INSERT INTO test VALUES (2, 'def');");
-  database.exec("INSERT INTO test VALUES (3, 'ghi');");
+    database.execute("INSERT INTO test VALUES (1, 'abc');");
+    database.execute("INSERT INTO test VALUES (2, 'def');");
+    database.execute("INSERT INTO test VALUES (?, 'ghi');", 3);
 
-  const auto sql_query = query{"SELECT * FROM test WHERE id != ?;", 2};
-  const auto results = database.execute<int, std::string>(sql_query);
-  const auto results2 = database.execute<int, std::string>(sql_query);
+    const auto sql_query = query{ "SELECT * FROM test WHERE id != ?;", 2 };
+    const auto results = database.execute<int, std::string>(sql_query);
+    const auto results2 = database.execute<int, std::string>(sql_query);
 }
--- sqlight.hpp
+++ sqlight.hpp
@@ -11,105 +11,126 @@
 #include <utility>
 #include <vector>
 
-namespace sqlight {
+namespace sqlight
+{
 
-template <typename...> class query;
+    template <typename... Args>
+    class query final
+    {
 
-class db final {
+    public:
+        explicit query(std::string query_str, Args... args)
+        : query_string{ std::move(query_str) }, query_args{ std::make_tuple(args...) }
+        {
+        }
 
-public:
-  explicit db(std::string_view db_file) {
-    const auto error = sqlite3_open(db_file.data(), &db_ptr); // TODO: Use v2
-    if (error != 0) {
-      sqlite3_close(db_ptr);
-    }
-  }
+        auto get_query_string() const
+        {
+            return query_string;
+        }
 
-  void exec(std::string_view query) {
-    char *err = nullptr;
-    sqlite3_exec(db_ptr, query.data(), nullptr, nullptr, &err);
-  }
+        auto get_query_args() const
+        {
+            return query_args;
+        }
 
-  template <typename... Args, template <typename...> typename Query_t,
-            typename... QueryArgs>
-    requires std::same_as<Query_t<QueryArgs...>, query<QueryArgs...>>
-  std::vector<std::tuple<Args...>>
-  execute(const Query_t<QueryArgs...> &sql_query) {
+    private:
+        std::string query_string;
+        std::tuple<Args...> query_args;
+    };
 
-    sqlite3_stmt *statement = nullptr;
-    sqlite3_prepare_v2(db_ptr, sql_query.query_string.data(),
-                       static_cast<int>(sql_query.query_string.size()),
-                       &statement, nullptr);
 
-    [&]<auto... Is>(std::index_sequence<Is...>) {
-      ((bind_param(statement, std::get<Is>(sql_query.query_args), Is + 1)),
-       ...);
-    }(std::index_sequence_for<QueryArgs...>{});
+    class db final
+    {
 
-    auto step = sqlite3_step(statement);
+    public:
+        explicit db(std::string_view db_file)
+        {
+            const auto error = sqlite3_open(db_file.data(), &db_ptr); // TODO: Use v2
+            if (error != 0)
+            {
+                sqlite3_close(db_ptr);
+            }
+        }
 
-    std::vector<std::tuple<Args...>> results;
+        template <typename... Args, template <typename...> typename Query_t, typename... QueryArgs>
+            requires std::same_as<Query_t<QueryArgs...>, query<QueryArgs...>>
+        [[maybe_unused]] std::vector<std::tuple<Args...>> execute(const Query_t<QueryArgs...>& sql_query)
+        {
 
-    while (step == SQLITE_ROW) {
-      auto row = std::tuple<Args...>{};
-      [&]<auto... Is>(std::index_sequence<Is...>) {
-        ((get_column(statement, Is, std::get<Is>(row))), ...);
-      }(std::index_sequence_for<Args...>{});
+            sqlite3_stmt* statement = nullptr;
+            sqlite3_prepare_v2(db_ptr, sql_query.get_query_string().data(),
+                               static_cast<int>(sql_query.get_query_string().size()), &statement, nullptr);
 
-      results.emplace_back(row);
+            [&]<auto... Is>(std::index_sequence<Is...>) {
+                ((bind_param(statement, std::get<Is>(sql_query.get_query_args()), Is + 1)), ...);
+            }(std::index_sequence_for<QueryArgs...>{});
 
-      step = sqlite3_step(statement);
-    }
+            auto step = sqlite3_step(statement);
 
-    sqlite3_finalize(statement);
+            std::vector<std::tuple<Args...>> results;
 
-    return results;
-  }
+            while (step == SQLITE_ROW)
+            {
+                auto row = std::tuple<Args...>{};
+                [&]<auto... Is>(std::index_sequence<Is...>)
+                { ((get_column(statement, Is, std::get<Is>(row))), ...); }(std::index_sequence_for<Args...>{});
 
-  ~db() {
-    if (db_ptr == nullptr) {
-      return;
-    }
-    sqlite3_close(db_ptr);
-  }
+                results.emplace_back(row);
 
-private:
-  template <typename T>
-  static void bind_param(sqlite3_stmt *statement, T &&arg, int index) {
-    using Arg_t = std::remove_cvref_t<T>;
+                step = sqlite3_step(statement);
+            }
 
-    if constexpr (std::is_same_v<std::int64_t, Arg_t>) {
-      sqlite3_bind_int64(statement, index, static_cast<std::int64_t>(arg));
-    } else if constexpr (std::is_integral_v<Arg_t>) {
-      sqlite3_bind_int(statement, index, static_cast<int>(arg));
-    }
-  }
+            sqlite3_finalize(statement);
 
-  template <typename T>
-  void get_column(sqlite3_stmt *statement, int index, T &value) {
-    if constexpr (std::is_integral_v<T>) {
-      value = sqlite3_column_int(statement, index);
-    }
-    if constexpr (std::is_same_v<std::string, T>) {
-      value = std::string{reinterpret_cast<const char *>(
-          sqlite3_column_text(statement, index))};
-    }
-  }
+            return results;
+        }
 
-  sqlite3 *db_ptr = nullptr;
-};
+        template <typename... Args, typename... QueryArgs>
+        [[maybe_unused]] std::vector<std::tuple<Args...>> execute(std::string_view sql_query, QueryArgs&&... args)
+        {
+            return execute<Args...>(query{ std::string{ sql_query }, args... });
+        }
 
-template <typename... Args> class query final {
-  friend db;
+        ~db()
+        {
+            if (db_ptr == nullptr)
+            {
+                return;
+            }
+            sqlite3_close(db_ptr);
+        }
 
-public:
-  explicit query(std::string query_str, Args... args)
-      : query_string{std::move(query_str)},
-        query_args{std::make_tuple(args...)} {}
+    private:
+        template <typename T>
+        static void bind_param(sqlite3_stmt* statement, T&& arg, int index)
+        {
+            using Arg_t = std::remove_cvref_t<T>;
 
-private:
-  std::string query_string;
-  std::tuple<Args...> query_args;
-};
+            if constexpr (std::is_same_v<std::int64_t, Arg_t>)
+            {
+                sqlite3_bind_int64(statement, index, static_cast<std::int64_t>(arg));
+            }
+            else if constexpr (std::is_integral_v<Arg_t>)
+            {
+                sqlite3_bind_int(statement, index, static_cast<int>(arg));
+            }
+        }
+
+        template <typename T>
+        void get_column(sqlite3_stmt* statement, int index, T& value)
+        {
+            if constexpr (std::is_integral_v<T>)
+            {
+                value = sqlite3_column_int(statement, index);
+            }
+            if constexpr (std::is_same_v<std::string, T>)
+            {
+                value = std::string{ reinterpret_cast<const char*>(sqlite3_column_text(statement, index)) };
+            }
+        }
+
+        sqlite3* db_ptr = nullptr;
+    };
 
 } // namespace sqlight
